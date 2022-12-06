cfd73ef404f494799b7bbb773bc8056c410c4669
Tamas Cservenak <tamas@cservenak.net>
2022-12-06 15:21:26 +0100

    Sign artifact

---
+++ maven-resolver-spi/src/main/java/org/eclipse/aether/spi/signature/SignatureAlgorithmFactorySelector.java
@@ -0,0 +1,61 @@
+package org.eclipse.aether.spi.signature;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Component performing selection of {@link SignatureAlgorithmFactory} based on known factory names.
+ * Note: this component is NOT meant to be implemented or extended by client, is exposed ONLY to make clients
+ * able to get {@link SignatureAlgorithmFactory} instances.
+ *
+ * @noimplement This interface is not intended to be implemented by clients.
+ * @noextend This interface is not intended to be extended by clients.
+ * @since 1.9.3
+ */
+public interface SignatureAlgorithmFactorySelector
+{
+    /**
+     * Returns factory for given algorithm name, or throws if algorithm not supported.
+     *
+     * @throws IllegalArgumentException if asked algorithm name is not supported.
+     */
+    SignatureAlgorithmFactory select( String algorithmName );
+
+    /**
+     * Returns a list of factories for given algorithm names in order as collection is ordered, or throws if any of the
+     * algorithm name is not supported. The returned list has equal count of elements as passed in collection of names,
+     * and if names contains duplicated elements, the returned list of algorithms will have duplicates as well.
+     *
+     * @throws IllegalArgumentException if any asked algorithm name is not supported.
+     * @throws NullPointerException if passed in list of names is {@code null}.
+     * @since 1.9.0
+     */
+    List<SignatureAlgorithmFactory> selectList( Collection<String> algorithmNames );
+
+    /**
+     * Returns a collection of supported algorithms. This set represents ALL the algorithms supported by Resolver,
+     * and is NOT in any relation to given repository layout used checksums, returned by method {@link
+     * org.eclipse.aether.spi.connector.layout.RepositoryLayout#getChecksumAlgorithmFactories()} (in fact, is super set
+     * of it).
+     */
+    Collection<SignatureAlgorithmFactory> getSignatureAlgorithmFactories();
+}
